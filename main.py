d9a449217d654ce2bcf86b45f0e12a1b7e6bd96b
Alice NOLLET <alice.nollet@student.junia.com>
2022-03-28 12:06:04 -0400

    modif religion

--- main.py
+++ main.py
@@ -11,24 +11,16 @@ testIndexes = pd.read_csv('exemple.txt', sep='\t', header=None)
 ''' Pipeline de prétraitrement '''
 
 # Age : Division des classes d'âges existantes en fonction de leur médiane
-col = "cps19_age"
-choices = [1,2,3,4,5,6]
-conditions = [(df[col]>=18) & (df[col]<29),
-            (df[col]>=29) & (df[col]<35),
-            (df[col]>=35) & (df[col]<45),
-            (df[col]>=45) & (df[col]<55),
-            (df[col]>=55) & (df[col]<66),
-            (df[col]>=66)]
-df["classeAge"] = np.select(conditions, choices, default=0)
-
-
 # Education : Numérisation/Ordonnement
 df['cps19_education']= df['cps19_education'].replace({'No schooling':0, 'Some elementary school':1, 'Completed elementary school':2,'Some secondary/ high school': 3, 'Completed secondary/ high school': 4, 'Some technical, community college, CEGEP, College Classique': 5, 'Completed technical, community college, CEGEP, College Classique': 6, 'Some university': 7, "Bachelor's degree": 8, "Master's degree":9, 'Professional degree or doctorate': 10, "Don't know/ Prefer not to answer": -1})
-print(df['cps19_education'])
+#print(df['cps19_education'])
 # Emploi : Remplacement de "je ne sais pas" par "autre"
 df['cps19_employment']= df['cps19_employment'].replace({"Don't know/ Prefer not to answer": 'Other'})
-print(df['cps19_employment'])
-# Religion : Groupement par "grand courant regligieux"
+#print(df['cps19_employment'])
+# Religion : Groupement par "grand courant regligieux" 
+#Non religieux/oriental/juif/musulman/chretiens/chretiens déviré/other
+df['cps19_religion']=df['cps19_religion'].replace({"None/ Don't have one/ Atheist":'Non religieux', 'Agnostic':'Non religieux', 'Buddhist/ Buddhism':'oriental', 'Hindu':'oriental', 'Jewish/ Judaism/ Jewish Orthodox':'juif', 'Muslim/ Islam': 'musulman', 'Sikh/ Sikhism':'oriental', "Anglican/ Church of England":'chretiens', 'Baptist':'chretiens', 'Catholic/ Roman Catholic/ RC':'chretiens', "Greek Orthodox/ Ukrainian Orthodox/ Russian Orthodox/ Eastern Orthodox": 'chretiens', "Jehovah's Witness":'chretiens', 'Lutheran':'chretiens', "Mormon/ Church of Jesus Christ of the Latter Day Saints":'chretiens déviré', 'Pentecostal/ Fundamentalist/ Born Again/ Evangelical':'chretiens déviré','Presbyterian':'chretiens','Protestant':'chretiens', 'United Church of Canada':'chretiens', 'Christian Reformed':'chretiens', 'Salvation Army':'chretiens', 'Mennonite':'chretiens déviré', 'Other (please specify)':'other',"Don't know/ Prefer not to answer":'other'})
+#print(df['cps19_religion'])
 
 ''' Séparation du dataset de test '''
 
