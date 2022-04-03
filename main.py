22b95fc84bbb28b2d17fb1542012b7fee3b2018b
Etienne <etienne.candat@viacesi.fr>
2022-04-02 20:23:32 -0400

    Implémentation de CategoricalNB

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import preprocessing
 from sklearn.model_selection import KFold
+from sklearn.naive_bayes import CategoricalNB
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score, f1_score
 
 ''' Paramétrage '''
 
@@ -24,7 +27,15 @@ df["classeAge"] = np.select(conditions, choices, default=0)
 
 # Education : Numérisation/Ordonnement
 df['cps19_education']= df['cps19_education'].replace({'No schooling':0, 'Some elementary school':1, 'Completed elementary school':2,'Some secondary/ high school': 3, 'Completed secondary/ high school': 4, 'Some technical, community college, CEGEP, College Classique': 5, 'Completed technical, community college, CEGEP, College Classique': 6, 'Some university': 7, "Bachelor's degree": 8, "Master's degree":9, 'Professional degree or doctorate': 10, "Don't know/ Prefer not to answer": -1})
-#print(df['cps19_education'])
+
+#print(df[df['cps19_education'] == -1])
+edu = np.array(df['cps19_education'])
+edu_mean = int(np.mean(edu[edu > -1]))
+#print(np.mean(edu[edu>-1]))
+#print(np.median(edu[edu>-1]))
+#df[df['cps19_education'] == -1]['cps19_education'] = int(np.mean(edu[edu > -1]))
+df.loc[df['cps19_education'] == -1, ['cps19_education']] = edu_mean
+
 # Emploi : Remplacement de "je ne sais pas" par "autre"
 df['cps19_employment']= df['cps19_employment'].replace({"Don't know/ Prefer not to answer": 'Other'})
 #print(df['cps19_employment'])
@@ -73,15 +84,26 @@ df['label'] = df['cps19_votechoice'] + df['cps19_votechoice_pr'] + df['cps19_vot
 
 attributes = [
     'classeAge',
-#    'cps19_gender',
+    'cps19_gender',
     'cps19_education',
-#    'cps19_employment',
-#    'cps19_religion',
+    'cps19_employment',
+    'cps19_religion',
     'label'
 ]
 
 df = df[attributes]
-df = df.join(pd.DataFrame(np.concatenate((onehotlabelsGenre, onehotlabelsRelig, onehotlabelsEmp), axis=1)))
+
+# Version avec les catégories
+gender_unq = list(df['cps19_gender'].unique())
+df['cps19_gender'] = df['cps19_gender'].apply(lambda x: gender_unq.index(x))
+religion_unq = list(df['cps19_religion'].unique())
+df['cps19_religion'] = df['cps19_religion'].apply(lambda x: religion_unq.index(x))
+emp_unq = list(df['cps19_employment'].unique())
+df['cps19_employment'] = df['cps19_employment'].apply(lambda x: emp_unq.index(x))
+
+# Version avec les vecteurs (recommenter les attributs correspondants plus haut)
+'''df = df.join(pd.DataFrame(np.concatenate((onehotlabelsGenre, onehotlabelsRelig, onehotlabelsEmp), axis=1)))
+df.columns = [str(col) for col in df.columns]'''
 
 ''' Séparation du dataset de test '''
 
@@ -93,10 +115,32 @@ dfTrain = dfTrain[dfTrain['label'] != '']
 
 ''' Entrainement '''
 
+catNB = CategoricalNB()
+rf = RandomForestClassifier()
+
 # Utilisation de "k-fold cross validation"
 kf = KFold(n_splits=10)
 for train_index, test_index in kf.split(dfTrain):
     print("TRAIN:", train_index, "TEST:", test_index)
+
+    X_train = dfTrain.iloc[train_index].loc[:, dfTrain.columns != 'label']
+    y_train = dfTrain.iloc[train_index]['label']
+    X_test = dfTrain.iloc[test_index].loc[:, dfTrain.columns != 'label']
+    y_test = dfTrain.iloc[test_index]['label']
+
+    # CategoricalNB
+    catNB.fit(X_train, y_train)
+    y_test_pred = catNB.predict(X_test)
+
+    print("Matrice de confusion :\n", confusion_matrix(y_test, y_test_pred))
+    print("Exactitude :", accuracy_score(y_test, y_test_pred))
+    print("Précision :", precision_score(y_test, y_test_pred, average='macro'))
+    print("Rappel :", recall_score(y_test, y_test_pred, average='macro'))
+    print("F1-score :", f1_score(y_test, y_test_pred, average='macro'))
+
+    # RandomForestClassifier
+    # rf.fit(X_train, y_train)
+    # y_test_pred = rf.predict(X_test)
 
     # Classificateur naïf de bayes
     # https://www.stat4decision.com/fr/foret-aleatoire-avec-python/
